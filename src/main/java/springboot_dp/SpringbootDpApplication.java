f2cb3e048a09c0c21abe07fdef48b1b907df231c
bxtsang <bxtsang.2018@sis.smu.edu.sg>
2022-06-09 23:17:55 +0800

    initial commit

---
+++ src/main/java/springboot_dp/SpringbootDpApplication.java
@@ -0,0 +1,13 @@
+package springboot_dp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springboot_dp/SpringbootDpApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot_dp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
